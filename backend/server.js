b141924ca16faa0ebd86e8c92cbaf0044ae42b7d
AyaanShaikh <officialayaanshaikh@gmail.com>
2025-12-07 19:42:44 +0530

    cron job implemented

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const path = require("path")
+const cron = require('node-cron');
 const PORT = 8000;
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+
+cron.schedule('*/10 * * * *', () => {
+  console.log('running a task every 15 minute');
+});
 
 // All routes
 app.use(require("./controllers/feedback"));
