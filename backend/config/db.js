4feacdd7111247fbd444993d2494626e1406e550
AyaanShaikh <officialayaanshaikh@gmail.com>
2025-12-07 17:52:52 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 let pool;
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
   // For Render or other hosted environments
   pool = new Pool({
     connectionString: process.env.DATABASE_URL_PRODUCTION,
