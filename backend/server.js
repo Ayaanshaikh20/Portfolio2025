5750e0957d9d8db315b588d8a2d69ebf3836813d
AyaanShaikh <officialayaanshaikh@gmail.com>
2025-12-07 19:19:53 +0530

    all changes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require("path")
 const { rateLimit } = require("express-rate-limit")
 const PORT = 8000;
 require("dotenv").config();
 
 const limiter = rateLimit({
-    windowMs: 5 * 60 * 1000, // 5 minutes
+    windowMs: 2 * 60 * 1000, // 5 minutes
     limit: 15, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
     standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
